12a6b77c452a7c78eac9faac264437e889f4ad8f
Gabor Javorszky <gabor.javorszky@ardanlabs.com>
2021-10-05 22:26:23 +0100

    Cleanup on aisle 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState, useRef } from 'react'
-
-import moment from 'moment';
+import { useState } from 'react'
 import './App.css'
 
 import NoCount from './NoCount';
 import Count from './Count';
 
-
 function App() {
   const minutes = 10;
   const [counting, setCounting] = useState(false)
 
-
   return (
-    <div className="App">
-      <header className="App-header">
-        {counting ?
-          <Count initial={minutes} /> :
-          <NoCount
-            cdMinute={String(minutes).padStart(2, 0)}
-            cdSecond="00"
-            cdMs="00"
-          />}
-        <div className="buttons">
-          {counting
-            ? <button className="reset" onClick={() => { setCounting(false) }}>Reset</button>
-            : <button className="yeet" onClick={() => { setCounting(true) }}>Yeet!</button>
-          }
-        </div>
-      </header>
-    </div>
+    <>
+      {counting ?
+        <Count initial={minutes} /> :
+        <NoCount
+          cdMinute={String(minutes).padStart(2, 0)}
+          cdSecond="00"
+          cdMs="00"
+        />}
+      <div className="buttons">
+        {counting
+          ? <button className="reset" onClick={() => { setCounting(false) }}>Reset</button>
+          : <button className="yeet" onClick={() => { setCounting(true) }}>Yeet!</button>
+        }
+      </div>
+    </>
   )
 }
 
